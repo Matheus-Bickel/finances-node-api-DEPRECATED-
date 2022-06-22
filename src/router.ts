21931c60666347383e10040aefa21ce8c3dea817
matheus bickel <102044460+mathbickel@users.noreply.github.com>
2022-06-22 17:38:06 -0300

    ajuste

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,11 @@
 import { Request, Response, Router } from 'express'
 import { app } from './app'
-import { SpentServiceImpl } from './Modules/Spents/Application/SpentsServiceImpl';
 
 const router = Router()
 app.use(router);
 
 router.get('/finances', (request: Request, response: Response) => {
-    response = new SpentServiceImpl()
+    return response.send({message: 'ELE GOSTAA'})
 })
 
 export { router }
