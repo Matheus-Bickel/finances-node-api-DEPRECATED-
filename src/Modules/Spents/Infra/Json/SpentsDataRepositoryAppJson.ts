854559d807be2e14d8e4d78ead6732cf99d06ff5
matheus bickel <102044460+mathbickel@users.noreply.github.com>
2022-07-02 13:12:09 -0300

    Voltando ao fluxo original onde todos os spents são salvos em um único json, por enquanto.

--- src/Modules/Spents/Infra/Json/SpentsDataRepositoryAppJson.ts
+++ src/Modules/Spents/Infra/Json/SpentsDataRepositoryAppJson.ts
@@ -2,35 +2,20 @@ import { injectable } from 'tsyringe'
 import { SpentsData } from '../../Domain/SpentsData'
 import { CreditSpentsDataToJsonAdapter } from '../Adapters/CreditSpentsDataToJsonAdapter'
 import { SpentsDataRepository } from '../../Domain/SpentsDataRepository'
-import { SpentTypeEnum } from '../../Domain/SpentTypeEnum'
-import { DebitSpentsDataToJsonAdapter } from '../Adapters/DebitSpentDataToJsonAdapter'
-import { PixSpentsDataToJsonAdapter } from '../Adapters/PixSpentsDataToJsonAdapter'
 
 import fs from 'fs'
-
 const spentsJson = require('../../../../../mocks/json/spents.json')
 
 @injectable()
 export class SpentsDataRepositoryAppJson implements SpentsDataRepository {
     async save(data: SpentsData[]): Promise<SpentsData[]> {
-        if (spentsJson.type === SpentTypeEnum.CREDIT) {
-            const toJson = CreditSpentsDataToJsonAdapter.from()
-            const jsonSpent = JSON.stringify(toJson.toJson(data))
-            fs.writeFile('mocks/json/CreditSpent.json', jsonSpent, (err) => {
-                if (err) {
-                    throw err
-                }
-            })
-            
-        } if (spentsJson.type === SpentTypeEnum.CREDIT) {
-            const toJson = DebitSpentsDataToJsonAdapter.from()
-            const jsonSpent = JSON.stringify(toJson.toJson(data))
-            fs.writeFile('mocks/json/DebitSpent.json', jsonSpent, (err) => {
-                if (err) {
-                    throw err
-                }
-            })
-        }
+        const toJson = CreditSpentsDataToJsonAdapter.from()
+        const jsonSpent = JSON.stringify(toJson.toJson(data))
+        fs.writeFile('mocks/json/AllSpents.json', jsonSpent, (err) => {
+            if (err) {
+                throw err
+            }
+        })
 
         return data
     } 
