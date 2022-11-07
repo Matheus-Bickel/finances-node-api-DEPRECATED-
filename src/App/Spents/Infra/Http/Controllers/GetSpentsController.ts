b2450f42cd05ca4c99f81f7ef7d41ec6e4c239ad
matheus <>
2022-11-07 16:28:53 -0300

    fix: npm install

--- src/App/Spents/Infra/Http/Controllers/GetSpentsController.ts
+++ src/App/Spents/Infra/Http/Controllers/GetSpentsController.ts
@@ -8,6 +8,6 @@ export class GetSpentsController implements Controller {
     async getSpent(req: Request, res: Response): Promise<SpentsData> {
         const data = GetSpentsServiceImpl.from(GetSpentsRepositoryJson.of())
 
-        return await res.send(await data.getData())
+        return res.send(await data.getData())
     }
 }
